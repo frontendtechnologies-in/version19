0bc01ece4b8157ffcf9340e3608c933a2cd7920d
frontendtechnologies-in <suryatej.t@frontendtechnologies.in>
2024-12-14 14:09:45 +0530

    Add files via upload

---
+++ src/app/main-nav/main-nav.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-main-nav',
+  imports: [RouterModule],
+  templateUrl: './main-nav.component.html',
+  styleUrl: './main-nav.component.scss'
+})
+export class MainNavComponent {
+
+}
